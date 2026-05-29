8380a2036eeab67b9c9d8d9e661c1610be90d197
Sachintechjoomla <92356209+Sachintechjoomla@users.noreply.github.com>
2026-05-29 14:43:21 +0530

    empty commit

--- generics/kafka/consumers/submissions.js
+++ generics/kafka/consumers/submissions.js
@@ -7,60 +7,68 @@
 
 //dependencies
 
-const userProjectsHelper = require(MODULES_BASE_PATH + '/userProjects/helper')
+const userProjectsHelper = require(MODULES_BASE_PATH + "/userProjects/helper");
 
 /**
- * submission consumer message received.
- * @function
- * @name messageReceived
- * @param {String} message - consumer data
- * @returns {Promise} return a Promise.
- */
+  * submission consumer message received.
+  * @function
+  * @name messageReceived
+  * @param {String} message - consumer data
+  * @returns {Promise} return a Promise.
+*/
+
 
 var messageReceived = function (message) {
-	return new Promise(async function (resolve, reject) {
-		try {
-			let parsedMessage = JSON.parse(message.value)
 
-			let submissionDocument = {
-				_id: parsedMessage._id.toString(),
-				status: parsedMessage.status,
-				completedDate: parsedMessage.submissionDate ? parsedMessage.submissionDate : '',
-				answers: parsedMessage.answers ? parsedMessage.answers : [],
-			}
+  return new Promise(async function (resolve, reject) {
 
-			await userProjectsHelper.pushSubmissionToTask(
-				parsedMessage.projectId,
-				parsedMessage.taskId,
-				submissionDocument
-			)
+    try {
+      
+      let parsedMessage = JSON.parse(message.value);
+      
+      let submissionDocument = {
+        "_id" : parsedMessage._id.toString(),
+        "status" : parsedMessage.status,
+        "completedDate" : parsedMessage.submissionDate ? parsedMessage.submissionDate : "",
+		"answers": parsedMessage.answers ? parsedMessage.answers : [],
+      };
 
-			return resolve('Message Received')
-		} catch (error) {
-			return reject(error)
-		}
-	})
-}
+      await userProjectsHelper.pushSubmissionToTask(
+        parsedMessage.projectId,
+        parsedMessage.taskId,
+        submissionDocument
+      );
+
+      return resolve("Message Received");
+    } catch (error) {
+      return reject(error);
+    }
+
+  });
+};
 
 /**
- * If message is not received.
- * @function
- * @name errorTriggered
- * @param {Object} error - error object
- * @returns {Promise} return a Promise.
- */
+  * If message is not received.
+  * @function
+  * @name errorTriggered
+  * @param {Object} error - error object
+  * @returns {Promise} return a Promise.
+*/
 
 var errorTriggered = function (error) {
-	return new Promise(function (resolve, reject) {
-		try {
-			return resolve(error)
-		} catch (error) {
-			return reject(error)
-		}
-	})
-}
+
+  return new Promise(function (resolve, reject) {
+
+    try {
+      return resolve(error);
+    } catch (error) {
+      return reject(error);
+    }
+
+  });
+};
 
 module.exports = {
-	messageReceived: messageReceived,
-	errorTriggered: errorTriggered,
-}
+  messageReceived: messageReceived,
+  errorTriggered: errorTriggered
+};
