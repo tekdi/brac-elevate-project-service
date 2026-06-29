0987804c1bc7bb1efe18ab956bf9dc7904890669
vaivk369 <vaishali.khandagale@tekditechnologies.com>
2026-06-29 16:55:01 +0530

    Issue #000 feat: allow supervisors to view the IDP of the participant

--- generics/services/programUsers.js
+++ generics/services/programUsers.js
@@ -4,7 +4,7 @@
  * created-date : 19-Jan-2026
  */
 
-const { result } = require('lodash')
+const { result, includes } = require('lodash')
 
 const programUsersQueries = require(DB_QUERY_BASE_PATH + '/programUsers')
 const userService = require(GENERICS_FILES_PATH + '/services/users')
@@ -178,7 +178,8 @@ module.exports = class ProgramUsersService {
 
 				if (
 					checkEntityHierarchy &&
-					(userId != entityDocData.hierarchy[0].id || userId != entityDocData.hierarchy[1].id)
+					userId != entityDocData.hierarchy[0].id &&
+					userId != entityDocData.hierarchy[1].id
 				) {
 					return {
 						status: 403,
